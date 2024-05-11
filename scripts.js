d50cb6ca4dcf70ce0386a89cb8bbcf028862e3ad
Ryan Evans <evans.ryan.p@gmail.com>
2024-05-10 23:02:42 -0500

    Added getComputerChoice and getHumanChoice functions
    The getComputerChoice and getHumanChoice functions have been added as well as initializing the player and computer scores for later use.
    
    The getComputerChoice function will auto-generate rock/paper/scissors based from a random number of 1-3. The getHumanChoice is generated by having the player enter a choice and validating that it is correct. It will also automatically change it to lowercase for easier processing.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,54 @@
-console.log("Hello World!");
+/*
+
+I'll plan and do pseudocoding here, as per instructed.
+
+Step 1
+-- Write the logic to get the computer choice --
+1. Create function named getComputerChoice
+2. Return a random value of rock, paper, or scissors.
+    Generate a random number 1-3 and assign each to either rock, paper, or scissors
+
+Step 2
+-- Write the logic to get the human choice --
+1. Create a function named getHumanChoice
+2. Ask the user to enter rock, paper, or scissors.
+3. If the user enters anything other than the three options, throw an error.
+
+Step 3
+-- Declare the players score variables -- 
+1. Create a variable to keep track of the human score
+2. Create a variable to keep track of the computer score
+3. Both scores should be 0 to start
+
+*/
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    console.log(computerChoice);
+    if (computerChoice === 1) {
+        computerChoice = "rock";
+    } else if (computerChoice === 2) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+
+    console.log("Computer Choice = " + computerChoice);
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Please enter rock, paper, or scissors.",).toLowerCase();
+    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+        console.log("Human Choice = " + humanChoice);
+        return humanChoice;
+    } else {
+        return getHumanChoice();
+    }
+}
+
+getComputerChoice();
+getHumanChoice();
